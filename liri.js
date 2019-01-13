d2c137c74f16bf258331f7fea2b183e9dab824d1
Dylan Thomson <djthomson88@gmail.com>
2019-01-12 21:07:05 -0500

    Comments, error handling, fixes, redo runCommand logic

--- liri.js
+++ liri.js
@@ -5,62 +5,49 @@ const axios = require("axios");
 const moment = require("moment");
 const fs = require("fs");
 
-// Get API keys
+// Gets API keys from .env file
 const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify);
 const bandsInTownID = keys.bandsInTown.id;
 const omdbID = keys.omdb.id;
 
-// Parse argument data
+// Get command to run from arguments
 const command = process.argv[2];
-let query = process.argv[3];
-for(let i = 4; i < process.argv.length; i++) {
-    query += "+" + process.argv[i];
+
+// Get the rest of the elements in process.argv and join them with "+" to build our query for API calls
+const query = process.argv.slice(3).join("+");
+
+// Define Command class
+class Command {
+    constructor(run) {
+        this.run = run;
+    }
+}
+
+// Define commands
+const commands = {
+    "spotify-this-song": new Command(logSpotifySongData),
+    "movie-this": new Command(logOMDBData),
+    "concert-this": new Command(logBandsInTownData),
+    "do-what-it-says": new Command(doWhatItSays)
 }
 
 // Use arguments to run a command
-runCommand(command, query);
-
-// Determine appropriate command and run
-function runCommand(command, query) {
-    if(command === "spotify-this-song") {
-        if(query) {
-            logSpotifySongData(query);
-        }
-        else {
-            console.log("Please enter a song name.\nExample: spotify-this-song chop suey");
-        }
-    }
-    else if(command === "concert-this") {
-        if(query) {
-            logBandsInTownData(query);
-        }
-        else {
-            console.log("Please enter a band name.\nExample: concert-this system of a down");
-        }
-    }
-    else if(command === "movie-this") {
-        if(query) {
-            logOMDBData(query);
-        }
-        else {
-            console.log("Please enter a movie name.\nExample: movie-this the shawshank redemption");
-        }
-    }
-    else if(command === "do-what-it-says") {
-        doWhatItSays();
-    }
-    else {
-        console.log("Valid commands:\nconcert-this\nspotify-this-song\nmovie-this\ndo-what-it-says")
-    }
+if(commands[command]) {
+    commands[command].run(query);
+}
+else {
+    printUsage();
 }
 
 // Use node-spotify-api to log song data
 function logSpotifySongData(query) {
+    if(!query) {
+        query = "The Sign Ace of Base";
+    }
     spotify.search({type: 'track', query: query, limit: 1}).then(response => {
         if(response.tracks.items.length < 1) {
-            console.log("I couldn't find the song you were looking for. Here is The Sign by Ace of Base");
-            logSpotifySongData("The Sign Ace of Base");
+            throw "I couldn't find the song you were looking for.";
         }
         else {
             const artistName = response.tracks.items[0].artists[0].name;
@@ -77,16 +64,19 @@ function logSpotifySongData(query) {
             console.log(output);
             logToFile("spotify-this-song", query, output);
         }
-    }).catch(err => {
-        console.log(err);
+    }).catch(error => {
+        console.log(error);
     });
 }
 
 // Use axios to log concert data for a band
 function logBandsInTownData(query) {
+    if(!query) {
+        query = "Red Hot Chili Peppers";
+    }
     axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=" + bandsInTownID).then(response => {
         if(!Array.isArray(response.data) || response.data.length < 1) {
-            console.log("I couldn't find concerts for the band you were looking for.");
+            throw "I couldn't find concerts for the band you were looking for.";
         }
         else {
             let output = "";
@@ -100,17 +90,19 @@ function logBandsInTownData(query) {
             console.log(output)
             logToFile("concert-this", query, output)
         }
-    }).catch(err => {
-        console.log(err);
+    }).catch(error => {
+        console.log(error);
     });
 }
 
 // Use axios to log movie data
 function logOMDBData(query) {
+    if(!query) {
+        query = "Mr. Nobody";
+    }
     axios.get("http://www.omdbapi.com/?t=" + query + "&plot=short&apikey=" + omdbID).then(response => {
         if(response.data.Response === 'False') {
-            console.log("I couldn't find the movie you were looking for. Here is Mr. Nobody");
-            logOMDBData("Mr. Nobody");
+            throw "I couldn't find the movie you were looking for.";
         }
         else {
             let output = "Title: " + response.data.Title;
@@ -131,31 +123,54 @@ function logOMDBData(query) {
             console.log(output);
             logToFile("movie-this", query, output);
         }
-    }).catch(err => {
-        console.log(err);
+    }).catch(error => {
+        console.log(error);
     });
 }
 
 // Run command located in random.txt
-function doWhatItSays() {
-    fs.readFile("random.txt", "utf8", (error, data) => {
-        if(error) throw error;
-        const dataArr = data.split(",");
-        const command = dataArr[0];
-        const query = dataArr[1];
-        if(command !== "do-what-it-says") { // We don't ever want to run this command because it will loop forever
-            fs.appendFile("log.txt", "do-what-it-says\n", error => {
-                if(error) throw error;
-            });
-            runCommand(command, query);
-        }
-    });
+function doWhatItSays(fileName) {
+    if(!fileName) {
+        fileName = "random.txt";
+    }
+    try {
+        fs.readFile(fileName, "utf8", (error, data) => {
+            if(error) throw error;
+            const dataArr = data.split(",");
+            const command = dataArr[0];
+            const query = dataArr[1];
+            if(command !== "do-what-it-says") { // We don't ever want to run this command because it will loop forever
+                fs.appendFile("log.txt", "do-what-it-says\n", error => {
+                    if(error) throw error;
+                });
+                commands[command].run(query);
+            }
+        });
+    }
+    catch(error) {
+        console.log(error);
+    }
 }
 
 // Append command and its result to log.txt
 function logToFile(command, searchTerm, result) {
-    const output = command + " " + searchTerm.replace("+", " ") + "\n" + result + "\n\n" + new Array(75).join("=") + "\n\n";
-    fs.appendFile("log.txt", output, error => {
-        if(error) throw error;
-    });
+    // Build presentable output to append by adding the command, searchTerm with + replaced by spaces, the result, and a divider
+    const output = command + " " + searchTerm.replace(/\+/g, " ") + "\n" + result + "\n\n" + new Array(75).join("=") + "\n\n";
+    try {
+        fs.appendFile("log.txt", output, error => {
+            if(error) throw error;
+        });
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+// Print information on how to use commands to console
+function printUsage() {
+    console.log("USAGE:");
+    console.log("node liri.js concert-this <artist/band name here>");
+    console.log("node liri.js spotify-this-song <song name here>");
+    console.log("node liri.js movie-this <movie name here>");
+    console.log("node liri.js do-what-it-says");
 }
